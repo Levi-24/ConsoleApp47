b990dcd1e5a6e0e02ecdd33f04a6f56edf805705
Levente Major <leventemajor36@gmail.com>
2023-09-11 09:01:29 +0200

    Add project files.

---
+++ ConsoleApp47/Kategoria.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _230905
+{
+    internal class Kategoria
+    {
+        public string KategoriaNeve { get; set; }
+        public int TulelokSzama { get; set; }
+        public int EltuntekSzama { get; set; }
+
+
+        public Kategoria(string beolvasottSor)
+        {
+            var darabok = beolvasottSor.Split(';');
+            this.KategoriaNeve = darabok[0];
+            this.TulelokSzama = int.Parse(darabok[1]);
+            this.EltuntekSzama = int.Parse(darabok[2]);
+        }
+    }
+}
---
+++ ConsoleApp47/Program.cs
@@ -0,0 +1,32 @@
+﻿using _230905;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace titanic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Kategoria> kategoriak = new List<Kategoria>();
+            var sr = new StreamReader(
+                path: @"..\..\..\src\titanic.txt",
+                encoding: Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                //string sor = sr.ReadLine();
+                //Kategoria kat = new Kategoria(sor);
+                //kategoriak.Add(kat);
+                kategoriak.Add(new Kategoria(sr.ReadLine()));
+            }
+
+            Console.WriteLine($"2.Feladat: {kategoriak.Count}db");
+
+
+            Console.ReadKey();
+        }
+    }
+}
