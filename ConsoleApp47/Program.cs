75bd678a6c678290bfb82cc7a55a4c778c4962f9
Levente Major <leventemajor36@gmail.com>
2023-09-12 07:59:36 +0200

    test commit

--- ConsoleApp47/Program.cs
+++ ConsoleApp47/Program.cs
@@ -28,7 +28,29 @@ namespace titanic
 
             var f3 = kategoriak.Sum(k => k.UtasokSzama);
             Console.WriteLine($"3.Feladat: {f3}fő");
-            
+
+            Console.WriteLine("4.Feladat Kulcsszó:");
+            string ksz = Console.ReadLine();
+
+            bool f4 = kategoriak.Any(k => k.KategoriaNeve.Contains(ksz));
+            Console.WriteLine($"\t{(f4 ? "Van" : "Nincs")} találat");
+
+            //int i = 0;
+            //while (i < kategoriak.Count() && !kategoriak[i].KategoriaNeve.Contains(ksz))
+            //{
+            //    i++;
+            //}
+            //if (i <kategoriak.Count)
+            //{
+            //    Console.WriteLine("\t van találat");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("\t nincs találat");
+            //}
+
+
+
             Console.ReadKey();
         }
     }
